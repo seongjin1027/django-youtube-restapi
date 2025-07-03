afc39d941894cc9a98e0e108b9c944c089afda28
한성진 <kinesis141@gmail.com>
2025-07-03 14:24:07 +0900

    db 세팅

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,14 +1,15 @@
-#wait_for_db
-# => DJango가 DB가 준비될 때까지 연결을 재시도하게 해주기 위해 필요.
-## => 왜 이렇게 하나요? 하나의 도커 이미지에 각 컨테이너(app, db)가 존재하기 때문에
+# wait_for_db
+# Django가 DB가 준비될 때까지 연결을 재시도하게 해주기 위해 필요.
+# 왜 이렇게 하나요? 하나의 도커 이미지에 각 컨테이너(app, db)가 존재하기 때문.
 
 import time
 
 from django.core.management.base import BaseCommand
-from django.db import connections # db랑 연결을 시도
+from django.db import connections  # db랑 연결을 시도
 
 from django.db.utils import OperationalError
 from psycopg2 import OperationalError as Psycopg2OperationalError
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -19,7 +20,7 @@ class Command(BaseCommand):
         while not is_db_connected:
             try:
                 is_db_connected = connections['default']
-            except(OperationalError, Psycopg2OperationalError):
+            except (OperationalError, Psycopg2OperationalError):
                 self.stdout.write("Retry DB Connection ...")
                 time.sleep(1)
 
--- app/core/tests.py
+++ app/core/tests.py
@@ -1,30 +1,29 @@
-
 from django.test import SimpleTestCase
 from unittest.mock import patch
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from psycopg2 import OperationalError as Psycopg2OperationalError
 
+
 @patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandsTests(SimpleTestCase):
 
-    # DB가  준비되었을 때 wait_for_db 잘 동작하는 지 체크하는 함수
+    # DB가 준비되었을 때 wait_for_db 잘 동작하는 지 체크하는 함수
     def test_wait_for_db_ready(self, patched_getitem):
-        patched_getitem.return_value =True
+        patched_getitem.return_value = True
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_getitem.call_count, 1)
 
-    # db연결에 오류가 생겼다고 가정을 하고 테스트
+    # db 연결에 오류가 생겼다고 가정하고 테스트
     @patch('time.sleep')
-    def test_wait_for_db_delay(self,patched_sleep, patched_getitem):
+    def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
         patched_getitem.side_effect = [Psycopg2OperationalError] + \
             [OperationalError] * 5 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_getitem.call_count, 7)
-
 
 # docker-compose run --rm app sh -c 'python manage.py test core'
